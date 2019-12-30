77632abca70c06974511a5e0039069219674eb79
agefly <wheatdew@163.com>
2019-12-30 12:44:55 +0800

    修改记录模块

---
+++ Assets/WheatDewCommunity/Log/ClearLogButton.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClearLogButton : MonoBehaviour
+{
+
+    public UnityEngine.UI.Text targetText;
+    void Start()
+    {
+        GetComponent<UnityEngine.UI.Button>().onClick.AddListener(delegate
+        {
+            targetText.text = "";
+        });
+    }
+
+}
