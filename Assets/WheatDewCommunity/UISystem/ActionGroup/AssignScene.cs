310e068ef1d37ffc8938115acf4d08fd679518b2
conlincat <conlincat@outlook.com>
2020-01-17 04:53:46 +0800

    背景

---
+++ Assets/WheatDewCommunity/UISystem/ActionGroup/AssignScene.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AssignScene : MonoBehaviour
+{
+    public string sceneName;
+    public BackgroundImageController bgiController;
+    private UnityEngine.UI.Button btn;
+
+    private void Start()
+    {
+        btn = GetComponent<UnityEngine.UI.Button>();
+        btn.onClick.AddListener(delegate { bgiController.UpdateBackground(sceneName); }); 
+    }
+}
