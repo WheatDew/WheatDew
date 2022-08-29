9ebe888df7cf3403bbb7cd5d87170df341d00e58
WheatDew <wheatdew@163.com>
2022-08-29 17:26:38 +0800

    科技树

--- Assets/SourceState/Basic/Data/TechnologySystem/Scripts/TechnologyPanel.cs
+++ Assets/SourceState/Basic/Data/TechnologySystem/Scripts/TechnologyPanel.cs
@@ -5,13 +5,24 @@ using UnityEngine.EventSystems;
 
 public class TechnologyPanel : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
+    public RectTransform target;
+    private bool isOver=false;
     public void OnPointerEnter(PointerEventData eventData)
     {
-        
+        isOver=true;
+
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
-        
+        isOver =false;
+    }
+
+    private void Update()
+    {
+        if (isOver && Input.GetMouseButton(2))
+        {
+            target.position += new Vector3(Input.GetAxisRaw("Mouse X")*100, Input.GetAxisRaw("Mouse Y")*100, 0);
+        }
     }
 }
