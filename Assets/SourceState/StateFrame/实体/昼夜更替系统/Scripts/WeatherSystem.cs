25fa3916a2c8f049a27508c669ecf2f7ec97c56f
WheatDew <wheatdew@163.com>
2022-09-05 17:29:04 +0800

    昼夜更替

--- Assets/SourceState/StateFrame/实体/昼夜更替系统/Scripts/WeatherSystem.cs
+++ Assets/SourceState/StateFrame/实体/昼夜更替系统/Scripts/WeatherSystem.cs
@@ -4,5 +4,12 @@ using UnityEngine;
 
 public class WeatherSystem : MonoBehaviour
 {
-    
+    public Light sun;
+    public float dayTime;
+    public float timer;
+
+    private void Update()
+    {
+        sun.transform.Rotate(Vector3.right, Time.deltaTime / dayTime*360f,Space.Self);
+    }
 }
