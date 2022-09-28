b36fddcddf4e0ae4726fd4cea16cbbf496571b38
WheatDew <wheatdew@163.com>
2022-09-28 17:29:22 +0800

    修改

--- Assets/SourceState/StateFrame/基础功能/Scripts/Component/CRule.cs
+++ Assets/SourceState/StateFrame/基础功能/Scripts/Component/CRule.cs
@@ -7,14 +7,29 @@ public class CRule : MonoBehaviour
     protected delegate string Rule();
     protected delegate string Result(string command);
     protected Dictionary<string, Rule> rules = new Dictionary<string, Rule>();
+    protected Dictionary<string, Result> results = new Dictionary<string, Result>();
 
     protected void SetRule(string key,Rule rule)
     {
         rules.Add(key,rule);
     }
 
-    protected Rule ApplyRule(string command)
+    protected string ApplyRule(string command)
     {
+        Result result = null;
+        string[] commands=command.Split('\n');
+        for(int i = 0; i < commands.Length; i++)
+        {
+            string[] slices = commands[i].Split(' ');
+            for(int j = 0; j < slices.Length; j++)
+            {
+                if (rules.ContainsKey(slices[j]))
+                    result += result;
+                //ToDo:
+            }
+        }
+
+
 
         return null;
         //return (string s) => { return ""; };
