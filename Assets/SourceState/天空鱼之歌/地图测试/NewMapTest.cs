9b90ef8fad5ccbb412f54643111a4af6c644ba98
WheatDew <wheatdew@163.com>
2022-12-25 23:03:52 +0800

    地图功能测试

--- Assets/SourceState/天空鱼之歌/地图测试/NewMapTest.cs
+++ Assets/SourceState/天空鱼之歌/地图测试/NewMapTest.cs
@@ -7,8 +7,12 @@ public class NewMapTest : MonoBehaviour
     public Texture texture;
     public string currentTexture;
     private Bounds bounds;
+    [System.NonSerialized] public Material material;
 
-
+    private void Start()
+    {
+        material = GetComponent<MeshRenderer>().material;
+    }
 
     private void OnMouseOver()
     {
@@ -16,7 +20,7 @@ public class NewMapTest : MonoBehaviour
 
         if (Input.GetMouseButtonDown(1))
         {
-            var material = GetComponent<MeshRenderer>().material;
+            
             material.mainTexture = texture;
             currentTexture = "xiaoxi";
             float max_x = -999, max_y = -999, min_x = 999, min_y = 999;
@@ -24,6 +28,8 @@ public class NewMapTest : MonoBehaviour
             for(int i = 0; i < NewMapSystem.instance.memeber.Length; i++)
             {
                 var item=NewMapSystem.instance.memeber[i];
+                if (item.currentTexture != "xiaoxi")
+                    continue;
                 if(item.transform.position.x-0.5f<min_x)
                 {
                     min_x = item.transform.position.x - 0.5f;
@@ -41,11 +47,19 @@ public class NewMapTest : MonoBehaviour
                     max_y = item.transform.position.y + 0.5f;
                 }
             }
-            Vector3 center = new Vector3((max_x + min_x) / 2f, (min_y + max_y) / 2f,0);
+            Vector3 center = new Vector3((max_x + min_x) / 2f, (min_y + max_y) / 2f, 0);
             Vector2 length = new Vector2(max_x - min_x, max_y - min_y);
-            center = new Vector3(center.x - length.x / 2f, center.y - length.y / 2f,0);
-            material.mainTextureOffset = new Vector2((transform.position.x - center.x)/length.x, (transform.position.y - center.y) / length.y);
-            material.mainTextureScale= new Vector2 (1f/length.x, 1f/length.y);
+            center = new Vector3(center.x - length.x / 2f, center.y - length.y / 2f, 0);
+
+            for (int i = 0; i < NewMapSystem.instance.memeber.Length; i++)
+            {
+                var item = NewMapSystem.instance.memeber[i];
+                if (item.currentTexture != "xiaoxi")
+                    continue;
+
+                item.material.mainTextureOffset = new Vector2((item.transform.position.x-0.5f - center.x) / length.x, (item.transform.position.y-0.5f - center.y) / length.y);
+                item.material.mainTextureScale = new Vector2(1f / length.x, 1f / length.y);
+            }
         }
     }
 }
