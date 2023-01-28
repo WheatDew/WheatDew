1bb11c76cc56727fc6e5767f14cd41a0306fddd3
WheatDew <wheatdew@163.com>
2023-01-28 17:30:27 +0800

    修改

--- Assets/SourceState/天空鱼之歌/人物/脚本/CharacterGroup.cs
+++ Assets/SourceState/天空鱼之歌/人物/脚本/CharacterGroup.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+public enum CharacterCreateMode {Random }
 public class CharacterGroup : MonoBehaviour
 {
     static CharacterGroup _s;
@@ -17,9 +19,46 @@ public class CharacterGroup : MonoBehaviour
     public Dictionary<string, CCharacter> characterList = new Dictionary<string, CCharacter>();
     public List<StrollPoint> strollPoints=new List<StrollPoint>();
 
+    public List<GameObject> characterPrefabs=new List<GameObject>();
+    public List<string> characterNames=new List<string>();
+    public Dictionary<string,GameObject> characterDict=new Dictionary<string, GameObject>();
+    public List<Transform> characterCreatePoints=new List<Transform>();
+
+    private void Start()
+    {
+        InitCharacterPrefabs();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            CreateCharacter("°×ÔªÔª",CharacterCreateMode.Random);
+        }
+    }
+
     public void BuildingTask()
     {
         
+    }
+
+    public void InitCharacterPrefabs()
+    {
+        for(int i = 0; i < characterPrefabs.Count; i++)
+        {
+            characterDict.Add(characterNames[i], characterPrefabs[i]);
+        }
+    }
+
+    public void CreateCharacter(string characterName,CharacterCreateMode characterCreateMode)
+    {
+        switch (characterCreateMode)
+        {
+            case CharacterCreateMode.Random:
+                GameObject obj = Instantiate(characterDict[characterName]);
+                obj.transform.GetChild(0).position = characterCreatePoints[Random.Range(0, characterCreatePoints.Count - 1)].position;
+                break;
+        }
     }
 
     public Vector3 GetStrollPoint()
